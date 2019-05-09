a4aa678598f168b74505acf138f02dfd6d80e6d5
2467233254@qq.com <2467233254@qq.com>
2019-05-09 18:48:34 +0800

    cc

--- config-server2/src/main/java/com/forezp/configserver2/ConfigServer2Application.java
+++ config-server2/src/main/java/com/forezp/configserver2/ConfigServer2Application.java
@@ -1,4 +1,4 @@
-package com.forezp.configserver;
+package com.forezp.configserver2;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -13,18 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableConfigServer
 @EnableEurekaClient
 @RestController
-public class ConfigServerApplication {
+public class ConfigServer2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigServer2Application.class, args);
     }
 
     @Value("${server.port}")
     String port;
 
     @RequestMapping("/hi")
-    public String hi(@RequestParam(defaultValue = "forezp", required = false) String name) {
-        return "Hi, "+name+"，This is port"+port;
+    public String hi(@RequestParam(required = false, defaultValue = "forezp") String name) {
+        return "Hi,"+name+"，This is port "+port;
     }
 
 }
